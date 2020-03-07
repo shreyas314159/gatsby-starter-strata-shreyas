8cd0bd70d501dfb25ef3c1e697e66ffbf26cacd6
Shreyas Prakash <shreyas314159@gmail.com>
2020-03-07 22:20:44 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ class HomeIndex extends React.Component {
                     </section>
 
                     <section id="three">
-                        <h2>Get In Touch</h2>
+                        <h2>Shreyas</h2>
                         <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
