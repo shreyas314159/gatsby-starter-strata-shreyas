9bd7e1d9a9e4eb3a2991cbc4e8bf787f2b2bf8b7
Shreyas Prakash <shreyas314159@gmail.com>
2020-03-08 12:30:03 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
+                            <h2>Shreyas<br />
                             magna sed accumsan arcu neque.</h2>
                         </header>
                         <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
